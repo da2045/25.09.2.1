736d5e351d55e2bdbbd175e1082aad7c24ea899b
da2045 <vitde2009@gmail.com>
2024-10-25 19:26:58 +0300

    Add project files.

---
+++ 25.09.2.1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //int a;
+    //int sum = 0;
+
+    //cout << "enter your number: ";
+    //cin >> a;
+
+    //for (int i = a; i <= 500; ++i) {
+    //    sum += i;
+    //}
+
+    //cout << " full number" << a << " to 500 = " << sum << endl;
+
+
+
+        //int x, y;
+        //int result = 1;
+
+        //cout << "enter x: ";
+        //cin >> x;
+        //cout << "enter y: ";
+        //cin >> y;
+
+        //for (int i = 0; i < y; ++i) {
+        //    result *= x;
+        //}
+
+        //cout << x << " in stepeni " << y << " = " << result << endl;
+
+    //int sum = 0;
+    //int count = 1000;
+
+    //for (int i = 1; i <= 1000; ++i) {
+    //    sum += i;
+    //}
+
+    //int middle = sum / count;
+
+    //cout << middle << endl;
+
+    int k;
+    cout << "enter k: ";
+    cin >> k;
+
+    for (int i = 1; i <= 10; ++i) {
+        cout << k << " x " << i << " = " << k * i << endl;
+    }
+
+    return 0;
+
+
+}
